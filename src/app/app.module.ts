15795cc73e4a225d88fda657e54731d935a7d440
Teela9 <motox4kalyani@gmail.com>
2021-08-14 23:42:58 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { CoursesService } from './courses/courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors/authors.service';
+import { ToggleStarComponent } from './toggle-star/toggle-star.component';
+import { TitleCasingComponent } from './title-casing/title-casing.component';
+import { TitleCasingPipe } from './title-casing/title-casing.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    AuthorsComponent,
+    ToggleStarComponent,
+    TitleCasingComponent,
+    TitleCasingPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
+  bootstrap: [
+    AppComponent,
+    CoursesComponent,
+    AuthorsComponent,
+    TitleCasingComponent,
+    ToggleStarComponent
+  ]
 })
+
+
 export class AppModule { }
